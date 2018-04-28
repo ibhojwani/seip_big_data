40d1025892d19b9030729a3019d92b22902bf0df
ibhojwani <ibhojwani@uchicago.edu>
2018-04-28 13:35:12 -0500

    fixed

--- pull_coords.py
+++ pull_coords.py
@@ -1,5 +1,8 @@
+'''
+'''
+
 import re
-from urllib2 import urlopen
+from requests import get
 from bs4 import BeautifulSoup
 
 
@@ -7,7 +10,10 @@ STARTING_URL = "http://www.unitar.org/unosat/maps/SYR"
 
 
 def pull_info():
-    initial_soup = BeautifulSoup(urlopen(STARTING_URL).read(), "html.parser")
+    '''
+    Build a list of dmg report objects w/ coords.
+    '''
+    initial_soup = BeautifulSoup(get(STARTING_URL).content(), "html.parser")
     link_list = initial_soup.find_all("a")
     damage_assessments = []
 
@@ -24,13 +30,15 @@ def pull_info():
 class DamageAssessment(object):
 
     def __init__(self, url):
-        self.test = self.title
         self.url = url
-        self.soup = BeautifulSoup(urlopen(url).read(), "html.parser")
+        self.soup = BeautifulSoup(get(url).content(), "html.parser")
         self.title = self.soup.title
         self.top_left, self.bottom_right = self.get_coords()
 
     def get_coords(self):
+        '''
+        Pulls top le ft and bottom right coords from page.
+        '''
         coords = re.findall(r"\d\d\.\d\d\d\d\d\d\d", str(self.soup))
 
         if len(coords) > 4:
