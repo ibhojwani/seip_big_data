2ef86d05141efcb08cdd28237156a6c4a4e433ad
tamos <tyler.amos@alumni.carleton.ca>
2018-04-29 14:06:00 -0500

    finally got pipe working

--- PlanetPipeline.py
+++ PlanetPipeline.py
@@ -8,10 +8,14 @@ from requests.auth import HTTPBasicAuth
 import os
 import requests
 import time
+from subprocess import run
+from tkinter import filedialog
+
 
 
 from settings import DEFAULT_ITEM_TYPE
 from settings import GEOJSON_DIRECTORY
+from settings import ACTIVATION_REQUEST
 
 class PlanetPipeline:
 # This is the planet pipeline
@@ -27,27 +31,26 @@ class PlanetPipeline:
             
         self.item_type = default_item_type
         self.client = api.ClientV1()
-        
-        self.geometries = []
-        self.load_city_boundaries(geojson_directory)
-        
-        self.search_results = []
-        
-    def load_city_boundaries(self, geojson_directory):
-        for i in os.listdir(geojson_directory):
-            with open(geojson_directory + "/" + i) as f:
-                # load geojson and take the parts we need to filter
-                loaded_file = json.load(f)['features'][0]['geometry']
-                self.geometries.append({i: loaded_file})
         self.geojson_dir = geojson_directory
+        
+        self.search_results = {}
+
 
     def search_all(self, **kwargs):
+        print(self.geojson_dir)
         for i in os.listdir(self.geojson_dir):
-                with open(self.geojson_dir + "/" + i) as f:
-                    self.search(f, **kwargs)
+            print(i, "isi")
+            with open(self.geojson_dir + "/" + i) as f:
+                geojson_content = json.load(f)['features'][0]['geometry']
+                result = self.search(geojson_file = geojson_content, **kwargs)
+                if result:
+                    self.search_results[result] = i
+        print(self.search_results)
+                    
 
     def make_filters(self, date_after, date_before, geojson_file,
                      cloud_threshold, resolution_threshold):
+        
         resolution = api.filters.range_filter('gsd', lt = resolution_threshold)
         
         # make cloud cover filters
@@ -58,8 +61,11 @@ class PlanetPipeline:
         end = api.filters.date_range('acquired', lt=date_before)
         
         # load geojson and take the parts we need to filter
-        aoi = json.load(geojson_file)['features'][0]['geometry']
-        place = api.filters.geom_filter(aoi)
+        #aoi = json.load(geojson_file)['features'][0]['geometry']
+
+
+        #raise TypeError
+        place = api.filters.geom_filter(geojson_file)
 
         # build a filter for the AOI
         query = api.filters.and_filter(place, start, end, clouds, resolution)
@@ -80,60 +86,54 @@ class PlanetPipeline:
         request = api.filters.build_search_request(query, item_types)
         # this will cause an exception if there are any API related errors
         results = self.client.quick_search(request, sort = "acquired asc")
+        
+        asset_type = 'analytic'
+        desired_item_type = "PSScene3Band"
 
-        if print_field != None:
-            # items_iter returns an iterator over API response pages
-            for item in results.items_iter(print_lim):
-              # each item is a GeoJSON feature
-              #print(item)
-              sys.stdout.write('%s\n' % item[print_field])
-            sys.stdout.write('\n')
-            
-        self.retrieval_list = []
-        for item in results.items_iter(print_lim):
-            self.fetch_trimmed_image(item)
-            break
-            
-    def fetch_trimmed_image(self, item):
-        asset_activated = False
-        count = 0
-        print("Starting to get asset")
-
-        while asset_activated == False:
-            count += 1
-            # Get asset and its activation status
+        for item in results.items_iter(4):
+            item_id = item['id']
+            item_type = item['properties']['item_type']
+            print(item_id, item_type)
             assets = self.client.get_assets(item).get()
-            asset = assets.get('analytic')
-            asset_status = asset["status"]
-    
-            # If asset is already active, we are done
-            if asset_status == 'active':
-                print("Asset is active and ready to download")
-                down_link = asset['location']
-                item_url = 'https://api.planet.com/data/v1/item-types/{}/items/{}/assets'.format(item_type, item_id)
-
-                # Request a new download URL
-                #result = requests.get(item_url, auth=HTTPBasicAuth(os.environ['PL_API_KEY'], ''))
-                #self.search_results = results
-                #print(retrieval_list) # keep results for later
-                vsicurl_url = '/vsicurl/' + down_link
-                output_file = item['id'] + '_subarea.tif'
-
-                # GDAL Warp crops the image by our AOI, and saves it
-                gdal.Warp(output_file, vsicurl_url, dstSRS = 'EPSG:4326', cutlineDSName = 'geojson_cities_2/map.geojson', cropToCutline = True)
-                break
-   
-            # Still activating. Wait and check again.
+        for asset in sorted(assets.keys()):
+            print(asset)
+            if asset == asset_type:
+                try:
+                    # request asset activation
+                    activation_request = item_id.join(ACTIVATION_REQUEST)
+                    response = run(activation_request, shell=True, check=True)
+                    return item_id
+                    
             else:
-                if count > 100:
-                    break
-                print("...Still waiting for asset activation...")
-                time.sleep(10)
+                return False
 
-                
+
+
+    def fetch_asset(self, item_id):
+
+        asset_type = 'analytic'
+        
+        desired_item_type = "PSScene3Band"
+        item_url = 'https://api.planet.com/data/v1/item-types/{}/items/{}/assets'.format(desired_item_type, item_id)
+
+        # Request a new download URL
+        result = requests.get(item_url, auth=HTTPBasicAuth(os.environ['PL_API_KEY'], ''))
+        # assemble urls and file paths
+        download_url = result.json()[asset_type]['location']
+        vsicurl_url = '/vsicurl/' + download_url
+        geom_id = str(self.search_results[item_id]).split(".")[0]        
+        output_file = "image_" + geom_id + "_" + item_id +  '_subarea.tif'
+        geojson_file = self.geojson_dir + "/" + self.search_results[item_id]
+        # get the image clipped to our geojson file size
+        gdal.Warp(output_file, vsicurl_url, dstSRS = 'EPSG:4326',
+                  cutlineDSName = geojson_file,
+                  cropToCutline = True)
+                         
 
 if __name__ == "__main__":
     p = PlanetPipeline()
     p.search_all(date_after = "2017-06-01", date_before = "2017-06-10",
                  print_field = 'id', cloud_threshold = 0.9,
-                 resolution_threshold = 3) 
+                 resolution_threshold = 3)
+    for i in p.search_results.keys():
+        p.fetch_asset(i)
--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@
 
 
 DEFAULT_ITEM_TYPE = ['PSScene4Band']
-GEOJSON_DIRECTORY = 'geojson_cities_2' 
+GEOJSON_DIRECTORY = 'geojson_cities_2'
+ACTIVATION_REQUEST = ['planet data download --activate-only --item-type PSScene3Band --asset-type analytic --string-in id', '2 --quiet']
