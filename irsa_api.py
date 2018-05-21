3ff038307ed91122aa1c4471bd521010d9ea2aa7
ibhojwani <ibhojwani@uchicago.edu>
2018-05-21 13:51:25 -0500

    added asynchronous download

--- irsa_api.py
+++ irsa_api.py
@@ -1,10 +1,12 @@
-from requests import get
 import csv
+from time import sleep
+from requests import get
 from astroquery.irsa import Irsa
+from xml.etree import ElementTree
 
-DATA_URL = "https://irsa.ipac.caltech.edu/TAP/sync?QUERY=SELECT+{0}+FROM+slphotdr4+WHERE+CONTAINS(POINT('J2000',ra,dec),CIRCLE('J2000',66.76957,26.10453,{1}))=1&FORMAT=CSV"
+DATA_URL = "https://irsa.ipac.caltech.edu/TAP/async?QUERY=SELECT+{0}+FROM+slphotdr4+WHERE+CONTAINS(POINT('J2000',ra,dec),CIRCLE('J2000',66.76957,26.10453,{1}))=1&FORMAT=CSV&PHASE=RUN"
 WGET = "https://irsa.ipac.caltech.edu/TAP/sync?QUERY=SELECT+*+FROM+slphotdr4+WHERE+CONTAINS(POINT('J2000',ra,dec),CIRCLE('J2000',66.76957,26.10453,1))=1&FORMAT=CSV"
-
+ALL_SKY = "https://irsa.ipac.caltech.edu/TAP/sync?QUERY=SELECT+{0}+FROM+slphotdr4FORMAT=CSV&PHASE=RUN"
 columns = [
     "ra",
     "dec",
@@ -29,11 +31,36 @@ def get_data(size, target):
     25 = 1.06 mil
     10 = 400k
     '''
+    assert (size >= 0), "Error: Size must be greater than 0"
     col_query = ",".join(columns)
     query = DATA_URL.format(col_query, size)
     print(query)
-    download = get(query, stream=True)
 
+    # Wait until API finishes the query
+    redirect = get(query, stream=True).url  # It redirects to info XML
+    info_tree = ElementTree.fromstring(get(redirect).content)
+    result_url = redirect + "/results/result"
+    status = info_tree[2].text
+
+    # Periodically check if query is done
+    while (status == "EXECUTING") or (status == "QUEUED"):
+        print("Working...")
+        sleep(10)
+        status = ElementTree.fromstring(get(redirect).content)[2].text
+
+    # Catch errors in query
+    if status == "ERROR":
+        result_tree = ElementTree.fromstring(get(result_url).content)
+        raise Exception(
+            "Query returned error: {}".format(result_tree[-1].text))
+    assert status != "ABORT", "Query was aborted"
+
+    # Write to file
+    print('Writing...')
+    result = get(result_url, stream=True)
     with open(target, "wb") as f:
-        for block in download.iter_content(1024):
+        for block in result.iter_content(1024):
             f.write(block)
+
+    size = result.headers["Content-length"]
+    print("Successfully downloaded {} MB".format(int(size)/1000000))
