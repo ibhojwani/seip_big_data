587e3bf5bf4b03adfb240c3f45e7b53942c83dc9
tamos <tyler.amos@alumni.carleton.ca>
2018-05-27 17:58:37 -0500

    adding edges and distance finder

--- edgesanddistance.py
+++ edgesanddistance.py
@@ -1,17 +1,28 @@
 from mrjob.job import MRJob
-from numpy import sqrt
+from mrjob.step import MRStep
+from numpy import sqrt, histogram, mean, array
 from numpy.random import randint
 from heapq import heappush, heappop
+from scipy.interpolate import UnivariateSpline
 
-# simple euclidean distance
-euc_dist = lambda x1, x2, y1, y2: sqrt((x1 - x2)**2 + (y1 - y2)**2)
+# simple euclidean distance in 4d space
+def euc_dist(x1, x2, y1, y2, z1, z2, w1, w2):
+    z1 = z1 / 3600000 # correct units
+    z2 = z2 / 3600000
+    w1 = w1 / 3600000
+    w2 = w2 / 3600000
+    a = (x1 - x2)**2 # right ascension
+    b = (y1 - y2)**2 # declination
+    c = (z1 - z2)**2 # proper motion right ascension
+    d = (w1 - w2)**2 # proper motion declination
+    return sqrt(sum([a, b, c, d]))
 
 # From heapq docs
 def heapsort(iterable):
     h = []
     for value in iterable:
         heappush(h, value)
-    return [heappop(h) for i in range(TOP_K)]
+    return h
 
 class FindEdgesAndDistance(MRJob):
 
@@ -23,39 +34,76 @@ class FindEdgesAndDistance(MRJob):
         # get the data
         vals = line.split(",")
         objid = vals[0]
-        heappush(self.node_list, (objid, vals))
-        self.list_len += 1
+        if objid != "designation": # header
 
-        # If our node list is too large, we randomly toss values
-        if self.list_len > K:
-            new_size = int(round(K * P))
-            remove = randint(low = 0, high = len(self.node_list),
-                                size = new_size)
-            tmp_nodes = [self.node_list[i] for i in remove]
-            self.node_list = tmp_nodes
-            self.list_len = new_size
-        # Validate that our list is still the right size
-        elif self.list_len > K * P:
-            # Now iterate over the nodes and find distances
-            for i in self.node_list:
-                dist = euc_dist(float(vals[1]), float(i[1][1]),
-                                float(vals[2]), float(i[1][2]))
-                dist2 = euc_dist(float(vals[2]), float(i[1][2]),
-                                float(vals[3]), float(i[1][3]))
-                if dist != dist2 != 0:
-                    yield objid, (dist, dist2, i[0])
-                    yield i[0], (dist, dist2, objid)
+            heappush(self.node_list, (objid, vals))
+            self.list_len += 1
 
-    def reducer(self, id, values):
-    	vals = heapsort(values)
-    	yield id, (vals) # get the TOP_K  closest objects
+            # If our node list is too large, we randomly toss values
+            if self.list_len > N:
+                new_size = int(round(N * P))
+                keep = randint(low = 0, high = len(self.node_list),
+                                    size = new_size)
+                tmp_nodes = [self.node_list[i] for i in keep]
+                self.node_list = tmp_nodes
+                self.list_len = new_size
+            # Validate that our list is still the right size
+            if self.list_len > N * P:
+                # Now iterate over the nodes and find distances
+                for i in self.node_list:
+                    x1 = float(vals[1]) # right ascension
+                    x2 = float(i[1][1]) # ditto
+                    y1 = float(vals[2]) # declination
+                    y2 = float(i[1][2]) # ditto 
+                    z1 = float(vals[5]) # motion in right ascension
+                    z2 = float(i[1][5]) # ditto
+                    w1 = float(vals[6]) # motion in declination
+                    w2 = float(i[1][6]) # ditto
+                    dist = euc_dist(x1, x2, y1, y2, z1, z2, w1, w2)
+                    if dist != 0: # not interested in self-dist
+                        yield objid, (dist, i[0])
+
+    def reducer(self, objid, values):
+        vals = heapsort(values)
+        cutoff = min(TOP_K, len(vals)) # ensure we don't have indexerrors
+        vals = vals[:cutoff]
+        # Ids are unique
+        distances = [i[0] for i in vals]
+        objects = [i[1] for i in vals]
+        counts, edges = histogram(distances, BINS, density = True)
+        yield objid, (tuple(counts), tuple(edges))
+
+    def mapper_2(self, objid, values):
+        counts, edges = values
+        y = array([0] + list(counts))
+        x = array(list(edges))
+        spl = UnivariateSpline(x = x, y = y, k = 4)
+        deltas = spl.derivative().roots()
+        try:
+            if deltas[0] < 1:
+                yield objid, (deltas[0])
+        except:
+            pass
+
+
+    def steps(self):
+        return [
+            MRStep(mapper_init = self.mapper_init,
+                    mapper = self.mapper,
+                   reducer = self.reducer),
+            MRStep(mapper = self.mapper_2),
+        ]
+
+
 
 if __name__ == '__main__':
-    K = 1000 # num. values to keep for comparing, 
-    P = 0.5 # factor by which to divide K
+    N = 5000 # num. values to keep for comparing, 
+    P = 0.5 # factor by which to divide N
     # think of P as the inverse proportion we keep when the list 
     # gets full, i.e., 0.25 -> 75% dropped
     # it is also the minimum number of points against which
     # we compare a node in order to create edges
-    TOP_K = 10 # the number of closest values we take
+    TOP_K = 1000 # the number of closest values we take
+    BINS = 40 # number of bins to histogram the points
+    SAMPLE_N = 2 # bandwidth for finding average
     FindEdgesAndDistance.run()
