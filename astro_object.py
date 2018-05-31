5e3bd9674a7dd34fb615137747f6c385177ed92f
Alexander Tyan <at1079@georgetown.edu>
2018-05-30 22:01:15 -0500

    # Fixed more astro_object.py

--- astro_object.py
+++ astro_object.py
@@ -45,23 +45,24 @@ class AstroObject:
         reader = csv.reader([data_row])
         line_list = next(reader)
 
-        self.objid = line_list[0]
-        self.ra = line_list[1]
-        self.dec = line_list[2]
-        self.ra_uncert = line_list[3]
-        self.dec_uncert = line_list[4]
-        self.ra_motion = line_list[5]
-        self.dec_motion = line_list[6]
-        self.ra_motion_uncert = line_list[7]
-        self.dec_motion_uncert = line_list[8]
-        self.w1 = line_list[9]
-        self.w2 = line_list[10]
-        self.w3 = line_list[11]
-        self.w4 = line_list[12]
-        self.w1_snr = line_list[13]
-        self.w2_snr = line_list[14]
-        self.w3_snr = line_list[15]
-        self.w4_snr = line_list[16]
+        if line_list[0] != 'designation':
+            self.objid = line_list[0]
+            self.ra = line_list[1]
+            self.dec = line_list[2]
+            self.ra_uncert = line_list[3]
+            self.dec_uncert = line_list[4]
+            self.ra_motion = line_list[5]
+            self.dec_motion = line_list[6]
+            self.ra_motion_uncert = line_list[7]
+            self.dec_motion_uncert = line_list[8]
+            self.w1 = line_list[9]
+            self.w2 = line_list[10]
+            self.w3 = line_list[11]
+            self.w4 = line_list[12]
+            self.w1_snr = line_list[13]
+            self.w2_snr = line_list[14]
+            self.w3_snr = line_list[15]
+            self.w4_snr = line_list[16]
 
     def __repr__(self):
         return self.objid
