e2a161b8b292e23c7681f8b48834aa2f282ac664
tamos <tyler.amos@alumni.carleton.ca>
2018-06-04 16:45:13 -0500

    some more changes to run

--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -17,13 +17,23 @@ gcloud dataproc clusters create star-finder #--num-workers 25  --worker-machine-
 #gcloud dataproc jobs submit pyspark --cluster=finding-stars src/outliers.py -- gs://astro-data/merged_seip.csv 2 gs://astro-data/clustered_points
 
 
+# run algorithm 1 - "obsolete implementation"
+#time python3 src/obsolete/edgesanddistance.py -r dataproc --cluster-id=star-finder test.csv > algo1results.csv 
+#--num-core-instances 25  --core-instance-type n1-standard-4 --instance-type n1-standard-4
+#--file src/astro_object.py  --file src/kmeans.py \
+#--file src/alg_2.py --file src/stdev.py --file src/alg_2_util.py --file src/alg_1_util.py \
+#--file src/astro_object.py \
+# --bootstrap-python --bootstrap-script=scripts/bootstrap.sh
+
+
 # run algorithm 1
-time python3 src/obsolete/edgesanddistance.py -r dataproc --cluster-id=star-finder test.csv > algo1results.csv 
---num-core-instances 25  --core-instance-type n1-standard-4 --instance-type n1-standard-4
+
+time python3 src/alg_1.py -r dataproc --cluster-id=star-finder test.csv > algo1results.csv \
 --file src/astro_object.py  --file src/kmeans.py \
 --file src/alg_2.py --file src/stdev.py --file src/alg_2_util.py --file src/alg_1_util.py \
 --file src/astro_object.py \
  --bootstrap-python --bootstrap-script=scripts/bootstrap.sh
+
 
 # run algorithm 2
 
