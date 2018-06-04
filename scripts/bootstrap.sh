3393a17085dc295860962678a901797818ed88cb
ibhojwani <ibhojwani@uchicago.edu>
2018-06-04 16:23:45 -0500

    deleted bad script

--- scripts/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install python3-pip -y
-sudo pip3 install mrjob
-sudo apt-get install python3-numpy -y
-sudo apt-get install python3-scipy -y
-sudo apt-get install python3-skimage -y
-sudo apt-get install python3-mrjob -y
