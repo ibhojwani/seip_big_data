d9825c8c67962411121111d13d425e817a350935
tamos <tyler.amos@alumni.carleton.ca>
2018-06-04 16:41:28 -0500

    correct

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,14 @@
+# This is the bootstrap file which we used to 
+# attempt to address installation issues with scikit-image
+# refer to the report 'challenges' for details
+
+sudo apt-get install python3-pip -y
+sudo pip install Cython 
+sudo pip3 install Cython
+#sudo apt-get install python-matplotlib python-numpy python-pil python-scipy -y
+sudo pip3 install mrjob
+sudo apt-get install python3-numpy -y
+sudo apt-get install python3-scipy -y
+#sudo pip3 install scikit-image==0.13.1
+#sudo apt-get purge python-requests -y # Suggested solution from S0; not sucessful
+#sudo apt-get install python-requests python-pip -y
